4f9525636c0e031ad3d61a8cbb161c7384ab66ef
Lucile <lucile.baudouin@gmail.com>
2021-06-23 08:50:24 +0200

    Resolution issues sur champs prenom, nom et fermeture du modal

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-GameOn - Créez une landing page avec Javascript Projet 4 du parcours developper front end OpenClassRooms
-Tests manuels
-Ajouter confirmation quand envoie réussi
-Ajouter validation ou messages d'erreur
+GameOn - Créez une landing page avec Javascript - Projet 4 du parcours developper front end OpenClassRooms
+
+# Issues to fix
 Implémenter entrées du formulaire
 Fermer la modale
+Tests manuels
+Ajouter confirmation quand envoi inscription réussi
+Ajouter validation ou messages d'erreur
--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,30 @@
+
+// =========================== DOM ELEMENTS =========================== //
+// global 
+const form = document.querySelector("#subscribe"); // __ form __ //
+const modalbg = document.querySelector(".bground"); // __ bg form __ //
+const modalBtn = document.querySelectorAll(".modal-btn"); // __ subscribe btn __ //
+const formData = document.querySelectorAll(".formData"); // __ div > inputs __ //
+const cross = document.querySelectorAll('.close'); // __ modal cross __ //
+
+// form elements
+const firstName = document.querySelector('#first'); //<input> prenom
+const lastName = document.querySelector('#last'); //<input> nom
+
+// status 
+let setting = {
+  firstName: {
+     data: '',
+     status: false
+  },
+  lastName: {
+     data: '',
+     status: false
+  }
+}
+
+// =========================== FUNCTIONS =========================== //
+// Responsive function
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,11 +34,36 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// Check first name function
+const checkInputFirstName = () => {
+  if (firstName.value.length > 2) {
+     setting.firstName.data = firstName.value;
+     setting.firstName.status = true;
+     firstName.parentNode.setAttribute("data-error-visible", "false");
+  } else {
+     setting.firstName.data = firstName.value;
+     setting.firstName.status = false;
+     firstName.parentNode.setAttribute("data-error-visible", "true");
+  }
+}
+firstName.addEventListener('input', checkInputFirstName);
 
+// Check last name function
+const checkInputLastName = () => {
+  if (lastName.value.length > 2) {
+     setting.lastName.data = lastName.value;
+     setting.lastName.status = true;
+     lastName.parentNode.setAttribute("data-error-visible", "false");
+  } else {
+     setting.lastName.data = lastName.value;
+     setting.lastName.status = false;
+     lastName.parentNode.setAttribute("data-error-visible", "true");
+  }
+}
+lastName.addEventListener('input', checkInputLastName);
+
+
+// =========================== EVENTS =========================== //
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +72,12 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form cross btn
+const closeModal = () => {
+  modalbg.style.display = "none";
+}
 
+// close modal event
+cross.forEach((span) => {
+  span.addEventListener("click", closeModal);
+})
